fe486473dd5200d292bab9a524f1477a5e7535cc
navisraditya <navisraditya@gmail.com>
2024-03-06 20:13:09 +0700

    fixed

--- scenes/FallingFish.gd
+++ scenes/FallingFish.gd
@@ -2,8 +2,6 @@ extends RigidBody2D
 
 export var sceneName = "LoseScreen"
 
-func _on_FallArea_body_entered(body):
+func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
 		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
-	else:
-		body.queue_free()
